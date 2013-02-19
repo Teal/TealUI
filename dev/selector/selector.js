e9e6a9b4d3e4d26402b9c0b441f63fe576d17ef6
xuld <xuld@vip.qq.com>
2013-02-19 21:55:17 +0800

    selector

--- dev/selector/selector.js
+++ dev/selector/selector.js
@@ -7,304 +7,351 @@ var fixGetElementById = true;
 
 function addElementsByTagName(elem, tagName, result) {
 
-	if (elem.getElementsByTagName) {
-		pushResult(elem.getElementsByTagName(tagName), result);
-	} else if (elem.querySelectorAll) {
-		pushResult(elem.querySelectorAll(tagName), result);
-	}
+    if (elem.getElementsByTagName) {
+        pushResult(elem.getElementsByTagName(tagName), result);
+    } else if (elem.querySelectorAll) {
+        pushResult(elem.querySelectorAll(tagName), result);
+    }
 
 }
 
 function pushResult(nodelist, result) {
-	for (var i = 0; nodelist[i]; i++) {
-		result[result.length++] = nodelist[i];
-	}
+    for (var i = 0; nodelist[i]; i++) {
+        result[result.length++] = nodelist[i];
+    }
 }
 
 Selector = {
 
-	/**
+    all: function (selector, context) {
+        
+    },
+
+    /**
 	 * 使用指定的选择器代码对指定的结果集进行一次查找。
 	 * @param {String} selector 选择器表达式。
 	 * @param {DomList/Dom} result 上级结果集，将对此结果集进行查找。
 	 * @return {DomList} 返回新的结果集。
 	 */
-	all: function (selector, context, result) {
+    query: function (selector, context) {
 
-		var match,
+        var result = new Dom(),
+            match,
 			value,
 			prevResult,
 			lastSelector,
 			elem,
 			i;
 
-		selector = selector.trim();
+        selector = selector.trim();
 
-		// 解析的第一步: 解析简单选择器
+        // 解析的第一步: 解析简单选择器
 
-		// ‘*’ ‘tagName’ ‘.className’ ‘#id’
-		if (match = /^(^|[#.])((?:[-\w]|[^\x00-\xa0]|\\.)+)$/.exec(selector)) {
+        // ‘*’ ‘tagName’ ‘.className’ ‘#id’
+        if (match = /^(^|[#.])((?:[-\w]|[^\x00-\xa0]|\\.)+)$/.exec(selector)) {
 
-			value = match[2].replace(rBackslash, "");
+            value = match[2].replace(rBackslash, "");
 
-			switch (match[1]) {
+            switch (match[1]) {
 
-				// ‘#id’
-				case '#':
+                // ‘#id’
+                case '#':
 
-					// 仅对 document 使用 getElementById 。
-					if (context.nodeType === 9) {
-						prevResult = context.getElementById(value);
-						if (prevResult && (!fixGetElementById || prevResult.getAttributeNode("id").nodeValue === value)) {
-							result[result.length++] = prevResult;
-						}
-						return result;
-					}
+                    // 仅对 document 使用 getElementById 。
+                    if (context.nodeType === 9) {
+                        prevResult = context.getElementById(value);
+                        if (prevResult && (!fixGetElementById || prevResult.getAttributeNode("id").nodeValue === value)) {
+                            result[result.length++] = prevResult;
+                        }
+                        return result;
+                    }
 
-					break;
+                    break;
 
-					// ‘.className’
-				case '.':
+                    // ‘.className’
+                case '.':
 
-					// 仅优化存在 getElementsByClassName 的情况。
-					if (context.getElementsByClassName) {
-						pushResult(context.getElementsByClassName(value), result);
-						return result;
-					}
+                    // 仅优化存在 getElementsByClassName 的情况。
+                    if (context.getElementsByClassName) {
+                        pushResult(context.getElementsByClassName(value), result);
+                        return result;
+                    }
 
-					break;
+                    break;
 
-					// ‘*’ ‘tagName’
-				default:
-					addElementsByTagName(context, value, result);
-					return result;
+                    // ‘*’ ‘tagName’
+                default:
+                    addElementsByTagName(context, value, result);
+                    return result;
 
-			}
+            }
 
-		}
+        }
 
-		// 解析的第二步: 获取所有子节点。并不断进行筛选。
+        // 解析的第二步: 获取所有子节点。并不断进行筛选。
 
-		prevResult = [context];
+        prevResult = [context];
 
-		// 解析分很多步进行，每次解析  selector 的一部分，直到解析完整个 selector 。
-		while (selector) {
+        // 解析分很多步进行，每次解析  selector 的一部分，直到解析完整个 selector 。
+        for (; ;) {
 
-			// 保存本次处理前的选择器。
-			// 用于在本次处理后检验 selector 是否有变化。
-			// 如果没变化，说明 selector 不能被正确处理，即 selector 包含非法字符。
-			lastSelector = selector;
+            // 保存本次处理前的选择器。
+            // 用于在本次处理后检验 selector 是否有变化。
+            // 如果没变化，说明 selector 不能被正确处理，即 selector 包含非法字符。
+            lastSelector = selector;
 
-			// 解析的第三步: 获取所有子节点。第四步再一一筛选。
-			// 针对子选择器和标签选择器优化(不需要获取全部子节点)。
+            // 解析的第三步: 获取所有子节点。第四步再一一筛选。
+            // 针对子选择器和标签选择器优化(不需要获取全部子节点)。
 
-			// ‘ selector’ ‘>selector’ ‘~selector’ ‘+selector’
-			if (match = /^\s*([>+~\s])\s*(\*|(?:[-\w*]|[^\x00-\xa0]|\\.)*)/.exec(selector)) {
+            // ‘ selector’ ‘>selector’ ‘~selector’ ‘+selector’
+            if (match = /^\s*([>+~\s])\s*(\*|(?:[-\w*]|[^\x00-\xa0]|\\.)*)/.exec(selector)) {
 
-				selector = RegExp.rightContext;
-				value = match[2].replace(rBackslash, "").toUpperCase() || "*";
+                selector = RegExp.rightContext;
+                value = match[2].replace(rBackslash, "").toUpperCase() || "*";
 
-				for (i = 0; elem = prevResult[i]; i++) {
-					switch (match[1]) {
-						case ' ':
-							addElementsByTagName(elem, value, result);
-							break;
+                for (i = 0; elem = prevResult[i]; i++) {
+                    switch (match[1]) {
+                        case ' ':
+                            addElementsByTagName(elem, value, result);
+                            break;
 
-						case '>':
-							for (elem = elem.firstChild; elem; elem = elem.nextSibling) {
-								if (elem.nodeType === 1 && (value === "*" || value === elem.tagName)) {
-									result[result.length++] = elem;
-								}
-							}
-							break;
+                        case '>':
+                            for (elem = elem.firstChild; elem; elem = elem.nextSibling) {
+                                if (elem.nodeType === 1 && (value === "*" || value === elem.tagName)) {
+                                    result[result.length++] = elem;
+                                }
+                            }
+                            break;
 
-						case '+':
-							while (elem = elem.nextSibling) {
-								if (elem.nodeType === 1) {
-									if ((value === "*" || value === elem.tagName)) {
-										result[result.length++] = elem;
-									}
-									break;
-								}
-							}
+                        case '+':
+                            while (elem = elem.nextSibling) {
+                                if (elem.nodeType === 1) {
+                                    if ((value === "*" || value === elem.tagName)) {
+                                        result[result.length++] = elem;
+                                    }
+                                    break;
+                                }
+                            }
 
-							break;
+                            break;
 
-						case '~':
-							while (elem = elem.nextSibling) {
-								if (elem.nodeType === 1 && (value === "*" || value === elem.tagName)) {
-									result[result.length++] = elem;
-								}
-							}
-							break;
+                        case '~':
+                            while (elem = elem.nextSibling) {
+                                if (elem.nodeType === 1 && (value === "*" || value === elem.tagName)) {
+                                    result[result.length++] = elem;
+                                }
+                            }
+                            break;
 
-						default:
-							throwError(match[0]);
-					}
-				}
+                        default:
+                            throwError(match[0]);
+                    }
+                }
 
 
-			} else {
+            } else {
 
-				// ‘tagName’ ‘*’ 
-				if (match = /^((?:[-\w\*]|[^\x00-\xa0]|\\.)+)/.exec(selector)) {
-					value = match[1].replace(rBackslash, "").toUpperCase();
-					selector = RegExp.rightContext;
-				}
+                // ‘tagName’ ‘*’ 
+                if (match = /^((?:[-\w\*]|[^\x00-\xa0]|\\.)+)/.exec(selector)) {
+                    value = match[1].replace(rBackslash, "").toUpperCase();
+                    selector = RegExp.rightContext;
+                }
 
-				for (i = 0; elem = prevResult[i]; i++) {
-					addElementsByTagName(elem, value || "*", result);
-				}
+                for (i = 0; elem = prevResult[i]; i++) {
+                    addElementsByTagName(elem, value || "*", result);
+                }
 
-			}
+            }
 
-			// 解析的第四步: 筛选第三步返回的结果。
+            if (prevResult.length > 1) {
+                result.unique();
+            }
 
-			// 如果没有剩余的选择器，说明节点已经处理结束。
-			if (selector) {
+            // 解析的第四步: 筛选第三步返回的结果。
 
-				// 进行过滤筛选。
-				selector = Selector.filter(selector, result);
+            // 如果没有剩余的选择器，说明节点已经处理结束。
+            if (selector) {
 
-				// 存储当前的结果值，用于下次继续筛选。
-				prevResult = result;
+                // 进行过滤筛选。
+                selector = Selector.filter(selector, result);
 
-				// 解析的第五步: 解析, 如果存在，则继续。
+            }
 
-				// ‘,selectpr’ 
-				if (match = /^\s*,\s*/.exec(selector)) {
-					ap.push.apply(result, Selector.all(RegExp.rightContext, context, []));
-					break;
-				}
+            // 如果筛选后没有其它选择器。返回结果。
+            if (!selector) {
+                break;
+            }
 
-				// 如果没有一个正则匹配选择器，则这是一个无法处理的选择器，向用户报告错误。
-				if (lastSelector.length === selector.length) {
-					throwError(selector);
-				}
-			}
-		}
+            // 解析的第五步: 解析, 如果存在，则继续。
 
-		return result;
-	},
+            // ‘,selectpr’ 
+            if (match = /^\s*,\s*/.exec(selector)) {
+                result.add(Selector.query(RegExp.rightContext, context)).unique();
+                break;
+            }
 
-	filter: function (selector, result) {
+            // 存储当前的结果值，用于下次继续筛选。
+            prevResult = result;
 
-		var match, filterFn, value;
+            // 清空之前的属性值。
+            result = new Dom();
 
-		// ‘#id’ ‘.className’ ‘:filter’ ‘[attr’
-		while (result.length && (match = /^([#\.:]|\[\s*)((?:[-\w]|[^\x00-\xa0]|\\.)+)/.exec(selector))) {
+            // 如果没有一个正则匹配选择器，则这是一个无法处理的选择器，向用户报告错误。
+            if (lastSelector.length === selector.length) {
+                throwError(selector);
+            }
+        }
 
-			selector = RegExp.rightContext;
+        return result;
+    },
 
-			filterFn = filterCache[match[0]];
+    filter: function (selector, result) {
 
-			// 如果不存在指定过滤器的特定函数，则先编译一个。
-			if (!filterFn) {
+        var match, filterFn, value;
 
-				filterFn = 'for(var n=0,i=0,e,t;e=r[i];i++){t=';
-				value = match[2].replace(rBackslash, "");
+        // ‘#id’ ‘.className’ ‘:filter’ ‘[attr’
+        while (result.length && (match = /^([#\.:]|\[\s*)((?:[-\w]|[^\x00-\xa0]|\\.)+)/.exec(selector))) {
 
-				switch (match[1]) {
+            selector = RegExp.rightContext;
 
-					// ‘#id’
-					case "#":
-						filterFn += 'Dom.getAttr(e,"id")===v;';
-						break;
+            filterFn = (Selector.filterFn || (Selector.filterFn = {}))[match[0]];
 
-						// ‘.className’
-					case ".":
-						filterFn += 'Dom.hasClass(e,v);';
-						break;
+            // 如果不存在指定过滤器的特定函数，则先编译一个。
+            if (!filterFn) {
 
-						// ‘:filter’
-					case ":":
+                filterFn = 'for(var n=0,i=0,e,t;e=r[i];i++){t=';
+                value = match[2].replace(rBackslash, "");
 
-						filterFn += Dom.pseudos[value] || throwError(match[0]);
+                switch (match[1]) {
 
-						// ‘selector:nth-child(2)’
-						if (match = /^\(\s*("([^"]*)"|'([^']*)'|[^\(\)]*(\([^\(\)]*\))?)\s*\)/.exec(selector)) {
-							selector = RegExp.rightContext;
-							value = match[3] || match[2] || match[1];
-						}
+                    // ‘#id’
+                    case "#":
+                        filterFn += 'Dom.getAttr(e,"id")===v;';
+                        break;
 
-						break;
+                        // ‘.className’
+                    case ".":
+                        filterFn += 'Dom.hasClass(e,v);';
+                        break;
 
-						// ‘[attr’
-					default:
-						value = [value.toLowerCase()];
+                        // ‘:filter’
+                    case ":":
 
-						// ‘selector[attr]’ ‘selector[attr=value]’ ‘selector[attr='value']’  ‘selector[attr="value"]’    ‘selector[attr_=value]’
-						if (match = /^\s*(?:(\S?=)\s*(?:(['"])(.*?)\2|(#?(?:[\w\u00c0-\uFFFF\-]|\\.)*)|)|)\s*\]/.exec(selector)) {
-							selector = RegExp.rightContext;
-							if (match[1]) {
-								value[1] = match[1];
-								value[2] = match[3] || match[4];
-								value[2] = value[2] ? value[2].replace(/\\([0-9a-fA-F]{2,2})/g, function (x, y) {
-									return String.fromCharCode(parseInt(y, 16));
-								}).replace(rBackslash, "") : "";
-							}
-						}
+                        filterFn += Dom.pseudos[value] || throwError(match[0]);
 
-						filterFn += 'Dom.getAttr(e,v[0])' + (Selector.relative[value[1]] || throwError(match[0]));
+                        // ‘selector:nth-child(2)’
+                        if (match = /^\(\s*("([^"]*)"|'([^']*)'|[^\(\)]*(\([^\(\)]*\))?)\s*\)/.exec(selector)) {
+                            selector = RegExp.rightContext;
+                            value = match[3] || match[2] || match[1];
+                        }
 
-				}
+                        break;
 
-				filterFn += 'if(t)r[n++]=e;}Array.prototype.splice.call(r,n);';
+                        // ‘[attr’
+                    default:
+                        value = [value.toLowerCase()];
 
-				filterFn = new Function('r', 'v', filterFn);
+                        // ‘selector[attr]’ ‘selector[attr=value]’ ‘selector[attr='value']’  ‘selector[attr="value"]’    ‘selector[attr_=value]’
+                        if (match = /^\s*(?:(\S?=)\s*(?:(['"])(.*?)\2|(#?(?:[\w\u00c0-\uFFFF\-]|\\.)*)|)|)\s*\]/.exec(selector)) {
+                            selector = RegExp.rightContext;
+                            if (match[1]) {
+                                value[1] = match[1];
+                                value[2] = match[3] || match[4];
+                                value[2] = value[2] ? value[2].replace(/\\([0-9a-fA-F]{2,2})/g, function (x, y) {
+                                    return String.fromCharCode(parseInt(y, 16));
+                                }).replace(rBackslash, "") : "";
+                            }
+                        }
 
-				filterFn.value = value;
-				
-			}
+                        filterFn += 'Dom.getAttr(e,v[0])' + (Selector.relative[value[1]] || throwError(match[0]));
 
-			filterFn(result, filterFn.value);
+                }
 
-		}
+                filterFn += 'if(t)r[n++]=e;}r.splice(n);';
 
-		return selector;
+                Selector.filterFn[match[0]] = filterFn = new Function('r', 'v', filterFn);
 
-	},
+                filterFn.value = value;
 
-	/**
+            }
+
+            filterFn(result, filterFn.value);
+
+        }
+
+        return selector;
+
+    },
+
+    match: function (elem, selector) {
+
+        if (elem.nodeType !== 1)
+            return false;
+
+        if (!elem.parentNode) {
+            var div = document.createElement('div');
+            div.appendChild(elem);
+            try {
+                return Dom.match(elem, selector);
+            } finally {
+                div.removeChild(elem);
+            }
+        }
+
+        var r, i = 0;
+        try {
+            r = node.parentNode.querySelectorAll(selector);
+        } catch (e) {
+            return query(selector, new Dom(node.parentNode)).indexOf(node) >= 0 || query(selector, Dom.document).indexOf(node) >= 0;
+        }
+        while (r[i])
+            if (r[i++] === node)
+                return true;
+
+        return false;
+
+        return match(elem, selector);
+    },
+
+    /**
 	 * 用于查找所有支持的伪类的函数集合。
 	 * @private
 	 * @static
 	 */
-	pseudos: {
-		target: 'window.location&&window.location.hash;t=t&&t.slice(1)===e.id',
-		empty: 'Dom.isEmpty(e)',
-		contains: 'Dom.getText(e).indexOf(v)>=0',
-		hidden: 'Dom.isHidden(e)',
-		visible: '!Dom.isHidden(e)',
+    pseudos: {
+        target: 'window.location&&window.location.hash;t=t&&t.slice(1)===e.id',
+        empty: 'Dom.isEmpty(e)',
+        contains: 'Dom.getText(e).indexOf(v)>=0',
+        hidden: 'Dom.isHidden(e)',
+        visible: '!Dom.isHidden(e)',
 
-		not: '!Dom.match(e, v)',
-		has: '!Dom.find(v, e)',
+        not: '!Dom.match(e, v)',
+        has: '!Dom.find(v, e)',
 
-		selected: 'Dom.attrHooks.selected.get(e, "selected", 1)',
-		checked: 'e.checked',
-		enabled: 'e.disabled===false',
-		disabled: 'e.disabled===true',
+        selected: 'Dom.attrHooks.selected.get(e, "selected", 1)',
+        checked: 'e.checked',
+        enabled: 'e.disabled===false',
+        disabled: 'e.disabled===true',
 
-		input: '^(input|select|textarea|button)$/i.test(e.nodeName)',
+        input: '^(input|select|textarea|button)$/i.test(e.nodeName)',
 
-		"nth-child": 'Dom.index(elem)+1;t=v==="odd"?t%2:v==="even"?t%2===0:t===v',
-		"first-child": '!Dom.prev(elem)',
-		"last-child": '!Dom.next(elem)',
-		"only-child": '!Dom.prev(elem)&&!Dom.next(elem)'
+        "nth-child": 'Dom.index(elem)+1;t=v==="odd"?t%2:v==="even"?t%2===0:t===v',
+        "first-child": '!Dom.prev(elem)',
+        "last-child": '!Dom.next(elem)',
+        "only-child": '!Dom.prev(elem)&&!Dom.next(elem)'
 
-	},
-	
-	relative: {
-		'undefined': '!=null;',
-		'=': '===v[2];',
-		'~=': ';t=(" "+t+" ").indexOf(" "+v[2]+" ")>=0;',
-		'!=': '!==v[2];',
-		'|=': ';t=("-"+t+"-").indexOf("-"+v[2]+"-")>=0;',
-		'^=': ';t=t&&t.indexOf(v[2])===0;',
-		'$=': ';t=t&&t.indexOf(v[2].length-t.length)===v[2];',
-		'*=': ';t=t&&t.indexOf(v[2])>=0;'
-	}
+    },
 
+    relative: {
+        'undefined': '!=null;',
+        '=': '===v[2];',
+        '~=': ';t=(" "+t+" ").indexOf(" "+v[2]+" ")>=0;',
+        '!=': '!==v[2];',
+        '|=': ';t=("-"+t+"-").indexOf("-"+v[2]+"-")>=0;',
+        '^=': ';t=t&&t.indexOf(v[2])===0;',
+        '$=': ';t=t&&t.indexOf(v[2].length-t.length)===v[2];',
+        '*=': ';t=t&&t.indexOf(v[2])>=0;'
+    }
 
 };
 
