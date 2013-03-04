4fd2a91eed8e2ac1482f798c89f24abb57e59bd3
xuld <xuld@vip.qq.com>
2013-03-04 14:24:27 +0800

    组件

--- src/dom/outerhtml.js
+++ src/dom/outerhtml.js
@@ -4,21 +4,29 @@
 
 //#include dom/base.js
 
-Dom.prototype.outerHtml = function () {
-	return this.access(function (elem) {
-		if ("outerHTML" in elem) {
-			return elem.outerHTML;
-		} else {
-			var div = Dom.getDocument(elem).createElement('div')
-			div.appendChild(Dom.clone(elem));
-			return div.innerHTML;
+Dom.prototype.outerHtml = function (value) {
+
+	if (arguments.length === 0) {
+		if (this.length) {
+			var elem = this[0];
+			if ("outerHTML" in elem) {
+				return elem.outerHTML;
+			} else {
+				var div = Dom.getDocument(elem).createElement('div')
+				div.appendChild(Dom.clone(elem));
+				return div.innerHTML;
+			}
 		}
-	}, function (elem, value) {
-		if ("outerHTML" in elem && !/<(?:script|style|link)/i.test(value)) {
-			elem.outerHTML = value;
-		} else {
-			Dom.before(elem, value);
-			Dom.remove(elem);
-		}
-	}, arguments, 0);
+
+		return null;
+	} else {
+		return this.iterate(function (elem) {
+			if ("outerHTML" in elem && !/<(?:script|style|link)/i.test(value)) {
+				elem.outerHTML = value;
+			} else {
+				Dom.before(elem, value);
+				Dom.remove(elem);
+			}
+		});
+	}
 };
