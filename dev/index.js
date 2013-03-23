5888e0660f175348ba2e7a5d08129e25d58f0e2c
xuld <xuld@vip.qq.com>
2013-03-23 13:57:10 +0800

    如何创建组件

--- dev/index.js
+++ dev/index.js
@@ -3,6 +3,8 @@ var DevIndex = {
 	'开发手册': '-',
 	'jPlusUI 设计理念': '~/dev/cookbooks/features.html',
 	'开发系统文档': '~/dev/cookbooks/apps.html',
+	'如何添加自己的模块': '~/dev/cookbooks/addmodule.html',
+	'如何开发 UI 组件': '~/dev/cookbooks/control.html',
 
 	'规范': '-',
 	'HTML 规范': '~/dev/specification/html.html',
