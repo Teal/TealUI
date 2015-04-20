3fc50741d8ab43939b8df3047aaef9dd338cea44
xuld <xuld@vip.qq.com>
2015-04-20 23:34:13 +0800

    panel    组件

--- src/container/panel.js
+++ src/container/panel.js
@@ -1,3 +1,17 @@
 ﻿
+$.fn.panelToggleCollapse = function (value) {
+    value = value === undefined ? this.hasClass('x-panel-collapsed') : !value;
+    if (value) {
+        this.addClass('x-panel-expanded').removeClass('x-panel-collapsed');
+        this.find('.x-panel-body').slideDown();
+    } else {
+        var me = this.addClass('x-panel-collapsing');
+        this.find('.x-panel-body').slideUp(function () {
+            me.addClass('x-panel-collapsed').removeClass('x-panel-expanded x-panel-collapsing');
+        });
+    }
+};
 
-$(document).on('click')
+$(document).on('click', '.x-panel-collapsed .x-panel-header, .x-panel-expanded .x-panel-header', function () {
+    $(this).parent().panelToggleCollapse();
+});
