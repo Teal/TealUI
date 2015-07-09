bab47d65cec2866bee2a3265bd632826e554513b
xuld <xuld@vip.qq.com>
2015-07-09 21:38:51 +0800

    控件

---
+++ src/typography/form/fileUpload.js
@@ -0,0 +1,22 @@
+/**
+ * @author xuld
+ */
+
+// #require ui/button/button.css
+// #require ui/form/fileupload.css
+// #require ui/core/base.js
+// #require ui/core/iinput.js
+
+var FileUpload = Control.extend({
+
+    init: function () {
+        var me = this;
+        this.elem.query('[type=file]').on('change', function () {
+            var textBox = me.elem.nextElementSibling;
+            if (textBox.matches('[type=text]') || ((textBox = me.elem.previousElementSibling) && textBox.matches('[type=text]'))) {
+                textBox.value = this.value;
+            }
+        });
+    }
+
+});
