705f14724a1f56a5add7516aa7d7ec59f897ffa8
xuld <xuld@vip.qq.com>
2016-06-03 11:54:49 +0800

    base64

--- src/utility/text/encoding/encodeBase64.ts
+++ src/utility/text/encoding/encodeBase64.ts
@@ -1,160 +1,48 @@
 ﻿
-
 /**
  * 对指定的字符串进行 Base64 编码(支持中文)。
  * @param value 要转换的字符串。
  * @returns 转换后的字符串。
- * @example encodeBase64("中文") // "LYc="
+ * @example encodeBase64("中文") // "5Lit5paH"
  */
-function encodeBase64(value: string) {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+export function encodeBase64(value: string) {
 
-    var out, i, len;
-    var c1, c2, c3;
-    len = value.length;
-    i = 0;
-    out = "";
-    while (i < len) {
-        c1 = value.charCodeAt(i++) & 0xff;
-        if (i == len) {
-            out += chars.charAt(c1 >> 2);
-            out += chars.charAt((c1 & 0x3) << 4);
-            out += "==";
-            break;
-        }
-        c2 = value.charCodeAt(i++);
-        if (i == len) {
-            out += chars.charAt(c1 >> 2);
-            out += chars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xf0) >> 4));
-            out += chars.charAt((c2 & 0xf) << 2);
-            out += "=";
-            break;
-        }
-        c3 = value.charCodeAt(i++);
-        out += chars.charAt(c1 >> 2);
-        out += chars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xf0) >> 4));
-        out += chars.charAt(((c2 & 0xf) << 2) | ((c3 & 0xc0) >> 6));
-        out += chars.charAt(c3 & 0x3f);
-    }
-    return out;
-}
-
-
-
-function utf16to8(value) {
-    var out, i, len, c;
-    out = "";
-    len = value.length;
-    for (i = 0; i < len; i++) {
-        c = value.charCodeAt(i);
-        if ((c >= 0x0001) && (c <= 0x007f)) {
-            out += value.charAt(i);
-        } else if (c > 0x07ff) {
-            out += String.fromCharCode(0xe0 | ((c >> 12) & 0x0f));
-            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3f));
-            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
+    // 转换 Unicode 字符为 UTF8 字符序列，确保每个字符都是 ASCII 字符。
+    let t: number[] = [];
+    for (let i = 0; i < value.length; i++) {
+        const c = value.charCodeAt(i);
+        if (c < 128) {
+            t.push(c);
+        } else if (c < 2048) {
+            t.push(192 | c >> 6 & 31, 128 | c >> 0 & 63);
         } else {
-            out += String.fromCharCode(0xc0 | ((c >> 6) & 0x1f));
-            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
-        }
-    }
-    return out;
-}
-
-
-var base64decodechars = [
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
-    -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
-    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1];
-
-f.output.value = base64encode(utf16to8(f.source.value))
-f.decode.value = utf8to16(base64decode(f.output.value))
-
-function base64decode(str) {
-    var c1, c2, c3, c4;
-    var i, len, out;
-
-    len = str.length;
-
-    i = 0;
-    out = "";
-    while (i < len) {
-
-        do {
-            c1 = base64decodechars[str.charCodeAt(i++) & 0xff];
-        } while (i < len && c1 == -1);
-        if (c1 == -1)
-            break;
-
-
-        do {
-            c2 = base64decodechars[str.charCodeAt(i++) & 0xff];
-        } while (i < len && c2 == -1);
-        if (c2 == -1)
-            break;
-
-        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
-
-
-        do {
-            c3 = str.charCodeAt(i++) & 0xff;
-            if (c3 == 61)
-                return out;
-            c3 = base64decodechars[c3];
-        } while (i < len && c3 == -1);
-        if (c3 == -1)
-            break;
-
-        out += String.fromCharCode(((c2 & 0xf) << 4) | ((c3 & 0x3c) >> 2));
-
-
-        do {
-            c4 = str.charCodeAt(i++) & 0xff;
-            if (c4 == 61)
-                return out;
-            c4 = base64decodechars[c4];
-        } while (i < len && c4 == -1);
-        if (c4 == -1)
-            break;
-        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
-    }
-    return out;
-}
-
-function utf8to16(str) {
-    var out, i, len, c;
-    var char2, char3;
-
-    out = "";
-    len = str.length;
-    i = 0;
-    while (i < len) {
-        c = str.charCodeAt(i++);
-        switch (c >> 4) {
-            case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-                // 0xxxxxxx
-                out += str.charAt(i - 1);
-                break;
-            case 12: case 13:
-                // 110x xxxx   10xx xxxx
-                char2 = str.charCodeAt(i++);
-                out += String.fromCharCode(((c & 0x1f) << 6) | (char2 & 0x3f));
-                break;
-            case 14:
-                // 1110 xxxx  10xx xxxx  10xx xxxx
-                char2 = str.charCodeAt(i++);
-                char3 = str.charCodeAt(i++);
-                out += String.fromCharCode(((c & 0x0f) << 12) |
-                    ((char2 & 0x3f) << 6) |
-                    ((char3 & 0x3f) << 0));
-                break;
+            t.push(224 | c >> 12 & 15, 128 | c >> 6 & 63, 128 | c >> 0 & 63);
         }
     }
 
-    return out;
+    // 一次编码 3 个字符，得到 4 位 Base64 编码，如果位数不够则补 '='。
+    const base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    const length = t.length;
+    let result = "";
+    for (let i = 0; i < length;) {
+
+        const c1 = t[i++];
+        result += base64EncodeChars.charAt(c1 >> 2);
+        if (i === length) {
+            result += base64EncodeChars.charAt((c1 & 3) << 4) + "==";
+            break;
+        }
+
+        const c2 = t[i++];
+        result += base64EncodeChars.charAt((c1 & 3) << 4 | (c2 & 240) >> 4);
+        if (i === length) {
+            result += base64EncodeChars.charAt((c2 & 15) << 2) + "=";
+            break;
+        }
+
+        const c3 = t[i++];
+        result += base64EncodeChars.charAt((c2 & 15) << 2 | (c3 & 192) >> 6) + base64EncodeChars.charAt(c3 & 63);
+
+    }
+    return result;
 }
